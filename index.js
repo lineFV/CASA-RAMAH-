88ffc536ea5362299f6b216bac421395e1d9e216
lineFV <aline.fer.val@gmail.com>
2018-10-23 09:02:33 -0300

    casa

---
+++ index.js
@@ -0,0 +1,18 @@
+const express=require('express');
+
+const app=express();
+app.set('view engine', 'ejs');
+app.use('/css', express.static('static'));
+
+app.get('/', (req,res)=>{
+    res.render('Home');
+});
+app.get('/Escolas',(req,res) =>{
+    res.render('Escolas');
+});
+app.get('/Turnos',(req,res) =>{
+    res.render('Turnos');
+});
+app.listen(4000, () =>{
+    console.log ("Servidor iniciado");
+});
